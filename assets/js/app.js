dfec74dbc7b6d352c4c205d7a9bc41f5dd6871a0
Bill Chen <meokey2000@gmail.com>
2011-08-03 12:59:03 -0400

    fiveth commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,7 @@ run(function () {
         	if (connectivity === NetworkStatus.NOT_REACHABLE) {
         		alert("No internet connection - we won't be able to show you any maps");
         	} else {
-        		alert("We can reach Google - get ready for some awesome maps!");
+        		//   alert("We can reach Google - get ready for some awesome maps!");
         	}
         });
     })();
@@ -39,7 +39,7 @@ run(function () {
             navigator.geolocation.getCurrentPosition(function (position) {
                 var location = "" + position.coords.latitude + "," + position.coords.longitude;
                 path += location + "&zoom=" + zoom;
-                path += "&size=250x250&maptype=" + map + "&markers=color:red|label:P|";
+                path += "&size=500x500&maptype=" + map + "&markers=color:red|label:P|";
                 path += location + "&sensor=false";
 
                 x$('img#static_map').attr('src', path);
